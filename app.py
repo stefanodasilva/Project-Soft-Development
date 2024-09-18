41968f1cbc089e74c31113aea15b3b49055caed2
stefano dasilva <stefanodasilva17@gmail.com>
2024-09-18 10:25:58 -0700

    Interactive Charts Project

---
+++ app.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+def load_data():
+    df = pd.read_csv("vehicles_us (2).csv")
+    return df
+
+df=load_data()
+
+st.title("Car Advertisement Dataset")
+
+st.header("Vehicle Price Distribution")
+
+fig_price = px.histogram(df, x="price", title="Distribution of Vehicle Prices", nbins=50)
+st.plotly_chart(fig_price)
+
+st.header("Price vs Odometer Scatter Plot")
+
+fig_scatter = px.scatter(df, x="odometer", y="price",
+                 title="Scatter Plot:Odometer vs Price",
+                 labels={"year":"Year of Manufacture", "price": "Price (USD)"})
+st.plotly_chart(fig_scatter)
+
+if st.checkbox("Show Raw Data"):
+    st.write("## Raw Dataset")
+    st.dataframe(df)
